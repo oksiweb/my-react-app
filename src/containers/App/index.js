8aafbea7bced6f5d1c9d6292eca379c2b5c0d15c
oksiweb <oksi.web@gmail.com>
2018-03-17 15:03:44 +0200

    create my-react-app

---
+++ src/containers/App/index.js
@@ -0,0 +1,21 @@
+// Core
+import React, { Component } from 'react';
+
+// Instruments
+import Styles from './styles';
+import moment from 'moment';
+
+
+export default class App extends Component {
+
+    timer = setInterval(() => this.forceUpdate(), 1000);
+
+    render () {
+        return (
+            <section className = { Styles.app }>
+                <h1>Welcome!</h1>
+                <p>It is {moment().format('MMMM D h:mm:ss a')}.</p>
+            </section>
+        );
+    }
+}
